3d5f93eacc22e821989d0d5463583e9f22f8d4d8
Will Beers <whbeers@gmail.com>
2023-03-04 23:16:47 -0800

    update script to extract revision

--- scripts/plot_schematic.sh
+++ scripts/plot_schematic.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-schematic_revision=${1}
 project=$(basename $(pwd))
 schematic_file="hardware/${project}.kicad_sch"
 
-if [[ -z "$schematic_revision" ]]; then
-  echo "No revision specified."
-  exit 1
-fi
-  
 if [[ -f "$schematic_file" ]]; then
+  schematic_revision=$(grep '\(rev \"\)' "$schematic_file" | awk -F\" '{print $2}') 
+  if [[ -z "$schematic_revision" ]]; then
+    echo "Could not determine revision."
+    exit 1
+  fi
+
   kicad-cli sch export pdf "hardware/${project}.kicad_sch" -o "schematics/${project}-${schematic_revision}.pdf"
   kicad-cli sch export svg "hardware/${project}.kicad_sch" -o schematics
   mv "schematics/${project}.svg" "schematics/${project}-${schematic_revision}.svg"
